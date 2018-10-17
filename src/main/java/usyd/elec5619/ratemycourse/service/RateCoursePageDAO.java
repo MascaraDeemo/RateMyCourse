0a61b46eb2cd0e143a4597e1984e10397a1b64f1
Sam Zhou <mascaradeemo@gmail.com>
2018-10-17 13:27:07 +1100

    config package created with content

--- src/main/java/usyd/elec5619/ratemycourse/service/RateCoursePageDAO.java
+++ src/main/java/usyd/elec5619/ratemycourse/service/RateCoursePageDAO.java
@@ -1,5 +1,11 @@
 package usyd.elec5619.ratemycourse.service;
 
-public class RateCoursePageDAO {
+import usyd.elec5619.ratemycourse.domain.RateCoursePage;
+import usyd.elec5619.ratemycourse.domain.RateCoursePageInfo;
 
+public class RateCoursePageDAO {
+	public RateCoursePage findRate(String id);
+	
+	
+	
 }
